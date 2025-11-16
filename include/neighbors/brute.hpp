c7036df92eb0bfd61e66d9b6208186f8db0fea93
sofos <97079659+161-de-bastos@users.noreply.github.com>
2025-11-15 22:20:08 -0500

    Brute parallelized

--- include/neighbors/brute.hpp
+++ include/neighbors/brute.hpp
@@ -2,9 +2,12 @@
 #include "neighbors/search.hpp"
 #include "distance.hpp"
 
-class bruteForce : public NeighborSearch {
+class BruteForce : public NeighborSearch {
 public:
-    explicit bruteForce(const Dataset& X);
+    explicit BruteForce(
+        const Dataset& X, 
+        ComputeMode mode = ComputeMode::Serial
+    );
 
     void radius_query(
         int idx, 
@@ -12,8 +15,23 @@ public:
         std::vector<int>& out_idx
     ) const override;
 
-    int size() const override;
+    int size() const override { 
+        return static_cast<int>(X_.size()); 
+    }
 
 private:
     const Dataset& X_;
+    ComputeMode mode_;
+
+    void serial(
+        int idx, 
+        real_t eps,
+        std::vector<int>& out_idx
+    ) const;
+
+    void omp(
+        int idx, 
+        real_t eps,
+        std::vector<int>& out_idx
+    ) const;
 };
